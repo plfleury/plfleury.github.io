192c37a99fac7038b1f338416ac05ef0a97261e7
Paul Fleury <fleury.paul@laposte.net>
2025-05-23 19:54:26 +0200

    Setup React + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold">Mon CV en React</h1>
+    </div>
+  )
+}
+
+export default App
